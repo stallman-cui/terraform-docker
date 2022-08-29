2c625c56dbba2c20ca62563d0102ffba9b6068fd
davidcui <liaoya.c@gmail.com>
2022-08-29 18:17:09 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,22 +15,44 @@ variable "name_length" {
 }
 
 resource "random_password" "password" {
-  length           = 16
+  length           = 20
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "random_password" "password_2" {
+  length           = 20
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
 resource "random_string" "random" {
-  length           = 16
+  length           = 20
   special          = true
   override_special = "/@£$"
 }
 
+resource "random_string" "random_2" {
+  length           = 20
+  special          = true
+  override_special = "/@£$"
+}
+
+
 resource "random_pet" "pet_name" {
+  length = var.name_length
+  separator = ":"
+}
+
+resource "random_pet" "pet_name_2" {
   length = var.name_length
   separator = "-"
 }
 
 output "pet_name" {
   value = random_pet.pet_name.id
+}
+
+output "pet_name_2" {
+  value = random_pet.pet_name_2.id
 }
